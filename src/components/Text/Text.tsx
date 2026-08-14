85110d2e7d7dd5eb17343a5b8c031cbd4e03cb33
boipelo <harryboipelo@icloud.com>
2026-08-15 00:59:29 +0200

    created a Text folder

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Prop = {
+
+        variant : string,
+        children ?: string,
+
+}
+export const Text = () => {
+
+
+  return (
+
+
+    <Text>
+        
+    </Text>
+  )
+}
